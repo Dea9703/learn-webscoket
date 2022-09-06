31a93ec5bfb915caa23689917d012832bff0df54
Dea9703 <dea9703@163.com>
2022-09-06 11:34:31 +0800

    feat: 新增Sec-WebSocket-Accept的计算

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,10 @@
 var app = require("express")();
 var server = require("http").Server(app);
 var WebSocket = require("ws");
+const crypto = require("crypto");
+
+const magic = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+const secWebSocketKey = "w4v7O6xFTi36lq3RNcgctw==";
 
 var wss = new WebSocket.Server({ port: 8080 });
 
@@ -17,6 +21,13 @@ wss.on("connection", function connection(ws) {
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "../index.html");
 });
+
+let secWebSocketAccept = crypto
+  .createHash("sha1")
+  .update(secWebSocketKey + magic)
+  .digest("base64");
+
+console.log(secWebSocketAccept); // Oy4NRAQ13jhfONC7bP8dTKb4PTU=
 
 app.listen(3000, () => {
   console.log("server on running");
